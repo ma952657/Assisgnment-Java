4508751bf909953d8bf45c91298dc344534baca7
Maheshwar Shahu <maheshwarshahu@Maheshwars-MacBook-Pro.local>
2021-06-17 02:42:03 +0100

    fixing .gitignore

--- src/main/java/com/example/demo/exception/OverDraftException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.exception;
-
-public class OverDraftException extends BusinessException {
-
-	public OverDraftException(String message, String errorCode) {
-		super(message, errorCode);
-	}
-}
